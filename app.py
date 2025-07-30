a90f125fc10e4e338d1cd7bb3b339612ee144121
Azharuddin Malik <azharuddinmalik13@gmail.com>
2025-07-30 20:34:38 +0530

    correcting the port issue , in app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def render_home_page():
     return render_template('MAD.html', title=PAGE_TITLE)
 
 if __name__ == '__main__':
-    app.run( port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
